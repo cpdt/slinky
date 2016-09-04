6ec018fefe426954d1873e40af39b4795a71c80c
cpdt <copodt@gmail.com>
2016-09-04 20:02:48 +1000

    new and improved installer

--- install-local.sh
+++ install-local.sh
@@ -3,24 +3,14 @@
 # Local installer - assumes all required files are in the current directory
 install_dir="/usr/local/bin"
 
-downloads=('slink' 'rmslink' 'lsslink' 'delslink' 'slinky-run.sh' 'relativepath.sh' 'slinky.cfg')
+echo "Starting Slinky local install from $(pwd) to $install_dir"
+
+downloads=('slink' 'rmslink' 'lsslink' 'delslink' 'slinky-run.sh' 'relativepath.sh' 'slinky.cfg' 'install-common.sh')
 
 for file in "${downloads[@]}"; do
-  echo "Copying ./$file to $install_dir/$file"
-  cp "./$file" "$install_dir/$file"
+  echo "Copying $(pwd)/$file to $install_dir/$file"
+  cp "$(pwd)/$file" "$install_dir/$file"
   chmod u+rx "$install_dir/$file"
 done
 
-echo "Creating Slinky command links for Windows use..."
-eval "$install_dir/slink" slink
-eval "$install_dir/slink" rmslink
-eval "$install_dir/slink" lsslink
-eval "$install_dir/slink" delslink
-
-echo "Finished installing Slinky!"
-echo
-echo "NEXT STEPS:"
-echo " 1. Add C:\\.slinky to your PATH in Windows"
-echo " 2. Run slink <command> to bind a command, and rmslink <command> to remove a command"
-echo " 3. Star Slinky on Github at https://github.com/cpdt/slinky"
-echo " 4. Enjoy your brand-new command-prompt powers :)"
+bash "$install_dir/install-common.sh"
--- install.sh
+++ install.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
 # Online installer - downloads required files from the Github repo
+version="1.1"
 install_dir="/usr/local/bin"
-download_location="https://raw.githubusercontent.com/cpdt/slinky/1.1"
+download_location="https://raw.githubusercontent.com/cpdt/slinky/$version"
 
-echo "Starting Slinky download from $download_location"
+echo "Starting Slinky download & install from $download_location to $install_dir"
 
-downloads=('slink' 'rmslink' 'lsslink' 'delslink' 'slinky-run.sh' 'relativepath.sh' 'slinky.cfg')
+downloads=('slink' 'rmslink' 'lsslink' 'delslink' 'slinky-run.sh' 'relativepath.sh' 'slinky.cfg' 'install-common.sh')
 
 for file in "${downloads[@]}"; do
   echo "Downloading $download_location/$file"
@@ -14,16 +15,4 @@ for file in "${downloads[@]}"; do
   chmod u+rx "$install_dir/$file"
 done
 
-echo "Creating Slinky command links for Windows use..."
-eval "$install_dir/slink" slink
-eval "$install_dir/slink" rmslink
-eval "$install_dir/slink" lsslink
-eval "$install_dir/slink" delslink
-
-echo "Finished installing Slinky!"
-echo
-echo "NEXT STEPS:"
-echo " 1. Add C:\\.slinky to your PATH in Windows"
-echo " 2. Use slink <command> to bind a command, and rmslink <command> to remove a command"
-echo " 3. Star Slinky on Github at https://github.com/cpdt/slinky"
-echo " 4. Enjoy your brand-new command-prompt powers :)"
+bash "$install_dir/install-common.sh"
