29c61b9c425a4e9fa227779975764f3b8aaf26f3
cpdt <copodt@gmail.com>
2016-09-10 15:23:36 +1000

    [fix] use install dir in slinky-run

--- slinky-run.sh
+++ slinky-run.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+source "$(dirname $0)/slinky.cfg"
+
 initial_directory=$PWD
 eval "$*"
-eval '/bin/bash $(dirname "$0")/relativepath.sh' "$initial_directory" "$PWD" > "/mnt/c/.slinky/.dirchange"
+bash "$(dirname "$0")/relativepath.sh" "$initial_directory" "$PWD" > "$install_dir/.dirchange"
